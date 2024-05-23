2f281e0a6873f829e65fe31a6249172a5243677b
Jannik Köster <koesterjannik1998@gmail.com>
2024-05-23 12:46:51 +0200

    add repo

--- src/components/ui/landing/Hero.tsx
+++ src/components/ui/landing/Hero.tsx
@@ -39,7 +39,12 @@ export function HeroSection() {
       </p>
       <TypewriterEffect words={words} />
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-10">
-        <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
+        <button
+          onClick={() =>
+            router.push("https://github.com/KoesterJannik/nextjs-starter-kit")
+          }
+          className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm"
+        >
           Check Repo
         </button>
         <button
