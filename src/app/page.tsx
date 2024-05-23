fffa93c13075b3934c04038fc760137c03529ab7
Jannik Köster <koesterjannik1998@gmail.com>
2024-05-23 12:53:05 +0200

    add github and discord

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,35 @@ import { SignIn } from "../components/ui/auth/sign-in";
 import { SparklesHeading } from "../components/ui/landing/Sparkling";
 import { HeroSection } from "../components/ui/landing/Hero";
 import { Features } from "../components/ui/landing/Features";
+import { FaDiscord, FaGithub } from "react-icons/fa"; // Import the Discord and GitHub icons
 
 type Props = {};
 
-function page({}: Props) {
+function Page({}: Props) {
   return (
-    <div className=" flex flex-col justify-center items-center min-h-screen">
+    <div className="relative flex flex-col justify-center items-center min-h-screen">
+      <div className="absolute top-4 right-4 flex space-x-4">
+        <a
+          href="https://discord.gg/FuNskDVvhd"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:text-blue-700 transition duration-200"
+        >
+          <FaDiscord size={30} />
+        </a>
+        <a
+          href="https://github.com/KoesterJannik/nextjs-starter-kit"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-800 hover:text-gray-600 transition duration-200"
+        >
+          <FaGithub size={30} />
+        </a>
+      </div>
       <HeroSection />
       <Features />
     </div>
   );
 }
 
-export default page;
+export default Page;
